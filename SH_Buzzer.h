20d1de3b98b0999b0571e0007b86c1a39150a5f8
SametHope <samethgoxx@gmail.com>
2024-04-21 16:49:52 +0300

    Rewrite SH_Buzzer

--- SH_Buzzer.h
+++ SH_Buzzer.h
@@ -1,18 +1,25 @@
-// OUTPUT COMPONENT
+// Digital Output Component
+// - `write(uint16_t frequency)`: Sets the buzzer to the specified frequency. Pass `0` to silence the buzzer.
+// - `silence()`: Stops the buzzer sound.
 struct SH_Buzzer {
   uint8_t pin;
-  unsigned int value;
+  uint16_t value;
 
   SH_Buzzer(uint8_t pin)
       : pin(pin), value(0) {
     pinMode(pin, OUTPUT);
   }
 
-  // Calls tone(pin, freq), set freq 0 for noTone()
-  void update(unsigned int freq) {
-    if (freq == 0)
-      noTone(pin);
-    else
-      tone(pin, freq);
+  void write(uint16_t frequency) {
+    value = frequency;
+    if (frequency != 0) {
+      tone(pin, frequency);
+    } else {
+      silence();
+    }
+  }
+
+  void silence() {
+    noTone(pin);
   }
 };
