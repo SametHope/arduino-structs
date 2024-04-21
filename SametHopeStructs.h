9b5de090486b19df35f0b10b8b094f511511d439
SametHope <samethgoxx@gmail.com>
2024-04-21 20:10:16 +0300

    Update "documentation"

--- SametHopeStructs.h
+++ SametHopeStructs.h
@@ -4,20 +4,17 @@
 // Common properties of the structures:
 // - Data structures are prefixed with SH_
 // - Data structures and the files they are declared on have matching names
-// - Data structures take required pins as input on declaration
+// - Data structures have documentation of their functions on the data structures definition
+// - Data structures take required pins as parameters on declaration
 // - Data structures store their own state
-// - Data structures initialise their pins on declaration
-// - State/variables of the data structures must not be modified directly but with functions
+// - Data structures setup their pin modes on declaration
+// - State/variables of the data structures should not be modified directly but with existing functions
 // Input components:
-// - SH_X.value is used to read the cached value of the components
-// - SH_X.update() must be called to update the state/value of the component
+// - SH_X.read() will update the values on the component and return the processed input if any
 // Output components:
-// - SH_X.value is used to read the cached value of the components
-// - SH_X.update(newValue) must be called to update the state/vale of the component
+// - SH_X.write(newValue) must be called to update the state/value of the component
 // Exceptions:
-// - SH_X.update(newValue) may have custom behaviours at certain values, those are likely commented
-// - Some data structures may have extra parameters on declarations which are also likely commented
-// - Some data structures such as joystick may store their value on different variables as they have multiple
+// - There are cases specific values may perform special actions which are commented/documented on structs definitions
 
 #include "SH_Button.h"
 #include "SH_Buzzer.h"
