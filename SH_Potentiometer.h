0b61881696bbcc8114c34bb75b846a3e5110ae3a
SametHope <samethgoxx@gmail.com>
2024-04-21 19:33:35 +0300

    Improve SH_Potentiometer

--- SH_Potentiometer.h
+++ SH_Potentiometer.h
@@ -1,11 +1,12 @@
 #include "SH_Utility.h"
 
 // Analog Input Component
-// Takes in min and max values on constructor to map the input range of 0...1023 to, inclusively.
-// - `read()`: Updates and returns the mapped value of the potentiometer within the specified range.
+// Takes in min and max values on constructor to map the input with.
+// - `read(unsigned int readAmount = 1)`: Reads input `readAmount` times and returns the mapped value of the median.
 struct SH_Potentiometer {
   uint8_t pin;
   int value;
+  int valueRaw;
   int minValue;
   int maxValue;
 
@@ -14,9 +15,31 @@ struct SH_Potentiometer {
     pinMode(pin, INPUT);
   }
 
-  int read() {
-    value = analogRead(pin);
-    value = mapInclusive(value, 0, 1024, minValue, maxValue);
+  int read(unsigned int readAmount = 1) {
+    // Take readAmount samples
+    int samples[readAmount];
+    for (int i = 0; i < readAmount; i++) {
+      samples[i] = analogRead(pin);
+    }
+
+    // Selection Sort
+    for (int i = 0; i < readAmount - 1; i++) {
+      int minIndex = i;
+      for (int j = i + 1; j < readAmount; j++) {
+        if (samples[j] < samples[minIndex]) {
+          minIndex = j;
+        }
+      }
+      if (minIndex != i) {
+        int temp = samples[i];
+        samples[i] = samples[minIndex];
+        samples[minIndex] = temp;
+      }
+    }
+
+    valueRaw = samples[readAmount / 2];
+    value = map(valueRaw, 0, 1023, minValue, maxValue);
+
     return value;
   }
 };
