8a949e7b11217bf58a9297fd0e59b64b979a1f5e
SametHope <samethgoxx@gmail.com>
2024-04-21 19:59:52 +0300

    Remove unused utilities

--- SH_Utility.h
+++ SH_Utility.h
@@ -1,13 +1,6 @@
 #ifndef SH_Utility
 #define SH_Utility
 
-// `out_min` Must be smaller than `out_max`
-long mapInclusive(long x, long in_min, long in_max, long out_min, long out_max) {
-  // Ensure x is within the input range
-  x = constrain(x, in_min, in_max);
-
-  // Calculate the mapped value inclusively
-  return ((x - in_min) * (out_max - out_min + 1)) / (in_max - in_min + 1) + out_min;
-}
+// Later
 
 #endif
