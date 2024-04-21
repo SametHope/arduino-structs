f579b492d1a1d9a6ef61bba48c3af6beda7d2d6a
SametHope <samethgoxx@gmail.com>
2024-04-21 16:53:04 +0300

    Rewrite SH_Joystick

--- SH_Joystick.h
+++ SH_Joystick.h
@@ -1,6 +1,11 @@
 #include "SH_Utility.h"
 
-// INPUT COMPONENT
+// Analog and Digital Input Component
+// Takes in min and max values for x and y on constructor to map their the input range of 0...1023 to.
+// - `read()`: Updates x, y, and click values.
+// - `readX()`: Reads and returns x-axis value.
+// - `readY()`: Reads and returns y-axis value.
+// - `readClick()`: Reads and returns click state.
 struct SH_Joystick {
   uint8_t xPin;
   uint8_t yPin;
@@ -18,29 +23,32 @@ struct SH_Joystick {
 
   // Map both x and y axis 0...1023 values to provided mins and max inclusively
   SH_Joystick(uint8_t xPin, uint8_t yPin, uint8_t clickPin, int xMin, int xMax, int yMin, int yMax)
-      : xPin(xPin), yPin(yPin), clickPin(clickPin), xMinValue(xMin), xMaxValue(xMax), yMinValue(yMin), yMaxValue(yMax) {
+      : xPin(xPin), yPin(yPin), clickPin(clickPin), xValue(0), yValue(0), clickValue(false), xMinValue(xMin), xMaxValue(xMax), yMinValue(yMin), yMaxValue(yMax) {
     pinMode(xPin, INPUT);
     pinMode(yPin, INPUT);
     pinMode(clickPin, INPUT_PULLUP);
   }
 
-  void update() {
-    updateX();
-    updateY();
-    updateClick();
+  void read() {
+    readX();
+    readY();
+    readClick();
   }
 
-  void updateX() {
+  int readX() {
     xValue = analogRead(xPin);
-    xValue = mapInclusive(xValue, 0, 1023, xMinValue, xMaxValue);
+    xValue = mapInclusive(xValue, 0, 1024, xMinValue, xMaxValue);
+    return xValue;
   }
 
-  void updateY() {
+  int readY() {
     yValue = analogRead(yPin);
-    yValue = mapInclusive(yValue, 0, 1023, yMinValue, yMaxValue);
+    yValue = mapInclusive(yValue, 0, 1024, yMinValue, yMaxValue);
+    return yValue;
   }
 
-  void updateClick() {
+  bool readClick() {
     clickValue = !digitalRead(clickPin);
+    return clickValue;
   }
 };
