b2aa791909159f05511bc6e1f1781b4558107ded
SametHope <samethgoxx@gmail.com>
2024-04-25 11:06:47 +0300

    Make SH_SingleColoredLED support PWM

--- SH_SingleColoredLED.h
+++ SH_SingleColoredLED.h
@@ -1,25 +1,27 @@
 // Digital Output Component
-// - `write(bool value)`: Sets the LED state to the specified value.
-// - `toggle()`: Toggles the LED state between ON and OFF.
+// - `writeDigital(bool value)`: Sets the LED state to the specified value (no PWM).
+// - `writeAnalog(uint8_t value)`: Sets the LED state to the specified value (PWM).
+// - `toggleDigital()`: Toggles the LED state between ON and OFF (no PWM).
 struct SH_SingleColoredLED {
   uint8_t pin;
-  bool value;
+  uint8_t value;
 
   SH_SingleColoredLED(uint8_t pin)
-      : pin(pin), value(false) {
+      : pin(pin) {
     pinMode(pin, OUTPUT);
   }
 
-  void write(bool value) {
+  void writeDigital(bool value) {
     this->value = value;
-    if (value) {
-      digitalWrite(pin, HIGH);
-    } else {
-      digitalWrite(pin, LOW);
-    }
+    digitalWrite(pin, value);
   }
 
-  void toggle() {
-    write(!value);
+  void writeAnalog(uint8_t value) {
+    this->value = value;
+    analogWrite(pin, value);
+  }
+
+  void toggleDigital() {
+    writeDigital(!value);
   }
 };
